958e4e0b622ae6dedf87e5bf623df06421b984c0
Dirk Agterhuis <dirkagterhuis@gmail.com>
2020-10-08 00:59:13 +0200

    feat: get address

--- force-app/main/default/lwc/companyAddressManager/companyAddressManager.js
+++ force-app/main/default/lwc/companyAddressManager/companyAddressManager.js
@@ -1,6 +1,8 @@
 import { LightningElement, api, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getNLastCreatedAccountsWithoutAddress from '@salesforce/apex/CompanyAddressManagerController.getNLastCreatedAccountsWithoutAddress';
 import getNLastModifiedAccountsWithAddress from '@salesforce/apex/CompanyAddressManagerController.getNLastModifiedAccountsWithAddress';
+import searchAddressForAccount from '@salesforce/apex/CompanyAddressManagerController.searchAddressForAccount';
 
 const columns = [
     { label: 'Company Name', fieldName: 'Name', sortable: true },
@@ -10,26 +12,65 @@ const columns = [
     { label: 'City', fieldName: 'BillingCity', sortable: true},
     { label: 'Country', fieldName: 'BillingCountry', sortable: true}
 ];
-const columnsWithGetAddressButton = [...columns, { label: 'Get Address', fieldName: 'TEST' }];
+const columnsWithGetAddressButton = [
+    ...columns,
+    {
+        type: 'button',
+        typeAttributes: {
+            label: 'Get Address',
+            name: 'getAddress',
+            title: 'Get address for this company based on the company name',
+            value: 'getAddress',
+        }
+    },
+];
 
 export default class CompanyAddressManager extends LightningElement {
     @api maxNumberOfAccountsWithoutAddress;
     @api maxNumberOfAccountsWithAddress;
     @track dataWithAddress;
     @track dataWithoutAddress;
-
     data = [];
     columns = columns;
     columnsWithGetAddressButton = columnsWithGetAddressButton;
 
     async connectedCallback() {
-        const dataWitoutAddress = await getNLastCreatedAccountsWithoutAddress({
+        const dataWithoutAddress = await getNLastCreatedAccountsWithoutAddress({
             recordLimit: this.maxNumberOfAccountsWithoutAddress
         });
-        this.dataWithoutAddress = dataWitoutAddress;
+        this.dataWithoutAddress = dataWithoutAddress;
         const dataWithAddress = await getNLastModifiedAccountsWithAddress({
             recordLimit: this.maxNumberOfAccountsWithAddress
         });
         this.dataWithAddress = dataWithAddress;
+    }
+
+    async callRowAction(event) {
+        const recordId = event.detail.row.Id;
+        const companyName = event.detail.row.Name;
+        if (event.detail.action.name === 'getAddress') {
+            searchAddressForAccount({
+                recordId: recordId,
+                accountName: companyName
+            })
+            .then(result => {
+                const evt = new ShowToastEvent({
+                    title: result,
+                    variant: 'success',
+                    mode: 'dismissable'
+                });
+                this.dispatchEvent(evt);
+                this.connectedCallback();
+            })
+            .catch(error => {
+                const evt = new ShowToastEvent({
+                    title: 'Application Error',
+                    message: error.body.message,
+                    variant: 'error',
+                    mode: 'dismissable'
+                });
+                this.dispatchEvent(evt);
+            });
+        }
     }
 }
